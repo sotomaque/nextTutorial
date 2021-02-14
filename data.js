fa2073cb9bcf45dbdbf0252b9cf6ceecbbd6e100
Enrique <sotomaque@gmail.com>
2021-02-14 13:00:53 -0800

    more playing around with nextjs api

---
+++ data.js
@@ -0,0 +1,16 @@
+export const articles = [
+  {
+    id: '1',
+    titie: 'demo title',
+    exceprt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    body:
+      'Et suscipit possimus voluptate, laudantium nisi voluptates neque veritatis eos qui exercitationem eveniet alias doloremque in velit ad maiores culpa sunt asperiores?',
+  },
+  {
+    id: '2',
+    titie: 'another title',
+    exceprt: 'Frank is a cat',
+    body:
+      'Et suscipit possimus voluptate, laudantium nisi voluptates neque veritatis eos qui exercitationem eveniet alias doloremque in velit ad maiores culpa sunt asperiores?',
+  },
+];
